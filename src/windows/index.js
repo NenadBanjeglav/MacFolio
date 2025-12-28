5bf333a5428059ea895559262dd1a3ff3d0b357e
Nenad <nenadmobmail@gmail.com>
2025-12-28 20:08:20 +0100

    implement many features

--- src/windows/index.js
+++ src/windows/index.js
@@ -2,7 +2,8 @@ import Terminal from "./Terminal";
 import Safari from "./Safari";
 import Resume from "./Resume";
 import Finder from "./Finder";
-import Text from "./text";
+import Text from "./Text";
 import Image from "./Image";
+import Contact from "./Contact";
 
-export { Terminal, Safari, Resume, Finder, Text, Image };
+export { Terminal, Safari, Resume, Finder, Text, Image, Contact };
