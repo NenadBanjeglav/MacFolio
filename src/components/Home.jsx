708887f08b5140ef431cf2f064038ef5f3a275fd
Nenad <nenadmobmail@gmail.com>
2026-06-07 17:44:11 +0200

    feat: add desktop start here file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { Draggable } from "gsap/Draggable";
 
 import useWindowStore from "#store/window";
 import useLocationStore from "#store/location";
-import { locations } from "#constants";
+import { locations, startHereFile } from "#constants";
 
 const projects = locations.work?.children ?? [];
 
@@ -17,8 +17,19 @@ const Home = () => {
     openWindow("finder");
   };
 
+  const handleOpenStartHere = () => {
+    openWindow("txtfile", startHereFile);
+  };
+
+  const handleStartHereKeyDown = (event) => {
+    if (event.key === "Enter" || event.key === " ") {
+      event.preventDefault();
+      handleOpenStartHere();
+    }
+  };
+
   useGSAP(() => {
-    Draggable.create(".folder", {
+    Draggable.create("#home .desktop-item", {
       cursor: "pointer",
       activeCursor: "grabbing",
     });
@@ -27,11 +38,22 @@ const Home = () => {
   return (
     <section id="home">
       <ul>
+        <li
+          className="group desktop-item start-here-cue cursor-pointer"
+          onClick={handleOpenStartHere}
+          onKeyDown={handleStartHereKeyDown}
+          role="button"
+          tabIndex={0}
+        >
+          <img src={startHereFile.icon} alt={`${startHereFile.name} file`} />
+          <p>{startHereFile.name}</p>
+        </li>
+
         {projects.map((project) => (
           <li
             key={project.id}
             className={clsx(
-              "group folder cursor-pointer",
+              "group desktop-item folder cursor-pointer",
               project.windowPosition,
             )}
             onClick={() => handleOpenProjectFinder(project)}
